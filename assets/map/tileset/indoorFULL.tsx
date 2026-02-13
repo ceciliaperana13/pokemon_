fa9bde8878fc895faf26050f839a2212f9373f48
ceciliaperana13 <cperana13@gmail.com>
2026-02-13 11:16:52 +0100

    choix du pokemon present mais pas actif

--- assets/map/tileset/indoorFULL.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="indoorFULL" tilewidth="16" tileheight="16" tilecount="76672" columns="32">
- <image source="indoorFULL.png" width="512" height="38336"/>
-</tileset>
--- fight/UIElement.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-class UIElement:
-    def __init__(self, x, y, width, height):
-        self.rect = pygame.Rect(x, y, width, height)
